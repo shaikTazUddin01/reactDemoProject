5609c0ef5ee3583cf773dd803e7545b654892652
taz <tazahmedsoft@gmail.com>
2024-12-04 10:13:14 +0600

    create react project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className="text-3xl bg-red-500">hi</h1>
+     <Outlet/>
+    </>
+  )
+}
+
+export default App
